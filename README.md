cf4d37e1d0b098170962d4179ec56bcc46405ca2
manymuch <zjx219@gmail.com>
2025-06-19 19:20:50 +0800

    Update to HA 2025.6.1

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # HisenseHA
 Home Assistant integration for Hisense smart device
+
+Only support AC currently.
+
+## Release
+### V1.3.1
+* Upgrade to Home Assistant 2025.6.1
+
+### V1.3.0
+* Support multiple AC
+
+### V1.2.0
+* Support login via username and password
--- custom_components/hisense/__init__.py
+++ custom_components/hisense/__init__.py
@@ -21,15 +21,9 @@ async def async_setup_entry(hass: core.HomeAssistant, entry: config_entries.Conf
             session=session
         )
 
-        for platform in ("climate", "switch", "button"):
-            hass.async_create_task(
-                hass.config_entries.async_forward_entry_setup(entry, platform)
-            )
-
+    # Load platforms
+    await hass.config_entries.async_forward_entry_setups(entry, ["climate", "switch", "button"])
     return True
 
 async def async_unload_entry(hass: core.HomeAssistant, entry: config_entries.ConfigEntry):
-    return all(
-        await hass.config_entries.async_forward_entry_unload(entry, platform)
-        for platform in ("climate", "switch", "button")
-    )
+    return await hass.config_entries.async_unload_platforms(entry, ["climate", "switch", "button"])
